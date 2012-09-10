f9ad8faab87bbc0660f143e2ede725b4ac78f462
skinnybuff <s_t_white@yahoo.com>
2012-09-09 22:31:58 -0400

    named all form elements
    added an object constructor and
    started serializing the forms

--- js/influenceMap.js
+++ js/influenceMap.js
@@ -3,10 +3,13 @@ jQuery(document).ready(function(){
 	// accordian storage array 
 	var sectionOdrer = [];
 	// pushes starting order to array
-	$(function(){
-		$('h3 a').each(function(index) {
+	$(function (){
+		$('h3 a').each(function(index, value) {
+			//making an object array from form data
+			$('form').each( console.log($(this).serializeArray()));
+			// pusshing section titles on the order array
 			sectionOdrer.push(index + ': ' + $(this).text());
-			//console.log(index + ': ' + $(this).text());
+			console.log(index + ': ' + $(this).text());
 		});	
 	});
 	// JQ ui accordian and drag and drop functionality
@@ -34,7 +37,6 @@ jQuery(document).ready(function(){
 		var sectionNumber = labelHolder.slice(5);
 		//adds the input value to the section header
 		$('a[id$="' + sectionNumber + '"]').html(this.value);
-		
 	});
 	
 	// a var to make the mouse move event boolean ** use with mouse down to target a drag **
@@ -46,8 +48,11 @@ jQuery(document).ready(function(){
 		if(drag){
 			// on release 
 			$(this).mouseup(function() {
-				// TODO:  update the array
-				console.log(this.innerHTML);				
+				// TODO:  update the array use sort to update
+				// sectionOdrer.sort(function(a,b){return b-a});
+				console.log(this.innerHTML);
+				// resort the array every time it rearranged
+
 			});
 		};
 		
@@ -57,7 +62,16 @@ jQuery(document).ready(function(){
 		using the largest image for testing
 	*/
 	$('.largestImage').attr("hover test");
-
+	
+	// object constructor for influence items
+	function influence(title,artist,url,order)
+	{
+		this.title=title;
+		this.artist=artist;
+		this.url=url;
+		this.order=order;
+	}
+	
 	
 
 });
